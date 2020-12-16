a2d687d82e40dc83589f302f234c680a763c1209
粽子 <rail_0039@126.com>
2020-12-17 01:45:49 +0800

    feature: priceList 组件构建和页面优化

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App_header mb-3">
+      <header className="App_header mb-4">
         <img src={bg} alt="背景图片"></img>
         <h2>React账本项目</h2>
         <p>从设计图到上线，精通组件化思维和组件测试，一步步进行迭代</p>
       </header>
+      {/*<nav className="container d-flex align-items-end text-center">*/}
+      {/*  23*/}
+      {/*</nav>*/}
       <div className="container">
+        {/*onEditorItem={Editor} onDeleteItem={Delete}*/}
         <PriceList PriceList={List}/>
       </div>
     </div>
--- src/components/PriceList/price-list.jsx
+++ src/components/PriceList/price-list.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const PriceList = ({PriceList}) => {
   return (
-    <ul>
+    <ul className="list-group">
       {
-        PriceList.map(item => <li key={item.id}>{item.title}</li>)
+        PriceList.map(item => <li className='list-group-item d-flex justify-content-between align-items-center' key={item.id}>
+          <span className='badge badge-primary col-1'>{item.category.name}</span>
+          <span className='col-4'>{item.title}</span>
+          <span className='col-2 font-weight-bolder'>
+            {
+              item.category.type === 'imooc' ? '+' : '-'
+            }
+            {item.price}元
+          </span>
+          <span className='col-2'>{item.dete}</span>
+          <button type="button" className="col-1 btn btn-outline-primary btn-sm" onClick={onEditorItem(33333)}>编辑</button>
+          <button type="button" className="col-1 btn btn-outline-danger btn-sm" onClick={onDeleteItem(444)}>删除</button>
+        </li>)
       }
     </ul>
   )
