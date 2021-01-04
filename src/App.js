d9aacec0ce28f61eac3725d8487340895c421ad2
粽子 <rail_0039@126.com>
2021-01-05 02:02:05 +0800

    fix: 首页优化和构建

--- src/App.js
+++ src/App.js
@@ -14,15 +14,28 @@ function App() {
       </header>
       <ul className="nav container nav-header justify-content-center">
         <li className="nav-item">
-          <Link to="/" className="nav-link active">Active</Link>
+          <Link to="/" className="nav-link active">列表模式</Link>
         </li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">Link</Link>
+          <Link to="/" className="nav-link">图表模式</Link>
         </li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">Disabled</Link>
+          <Link to="/" className="nav-link">TODO表单</Link>
         </li>
       </ul>
+      <div className="jumbotron container border border-info">
+        <h1 className="display-12 text-center font-weight-bolder">Keep 记账</h1>
+        <p className="lead">实时记录每天和每月账单信息并且做一定的汇总</p>
+        <hr className="my-2" />
+        <p className="text-secondary font-weight-bolder">选择月份</p>
+        <div className="d-flex align-items-center justify-content-between">
+          <button type="button" className="btn btn-primary">Primary</button>
+          <div className="sum-money text-right align-middle">
+            <span className="col px-md-5"><b className="text-info">收入：</b> 90</span>
+            <span><b className="text-info">支出：</b> 200</span>
+          </div>
+        </div>
+      </div>
       <div className="container">
         {/* onEditorItem={Editor} onDeleteItem={Delete} */}
         <PriceList PriceList={List} />
--- src/components/PriceList/price-list.jsx
+++ src/components/PriceList/price-list.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const PriceList = ({PriceList}) => {
+const PriceList = ({ PriceList }) => {
   return (
-    <ul className="list-group">
-      {
-        PriceList.map(item => <li className='list-group-item d-flex justify-content-between align-items-center' key={item.id}>
-          <span className='badge badge-primary col-1'>{item.category.name}</span>
-          <span className='col-4'>{item.title}</span>
-          <span className='col-2 font-weight-bolder'>
-            {
-              item.category.type === 'imooc' ? '+' : '-'
-            }
-            {item.price}元
-          </span>
-          <span className='col-2'>{item.dete}</span>
-          <button type="button" className="col-1 btn btn-outline-primary btn-sm">编辑</button>
-          <button type="button" className="col-1 btn btn-outline-danger btn-sm">删除</button>
-        </li>)
-      }
-    </ul>
-  )
-}
+    <>
+      <ul className="list-group">
+        {PriceList.map((item) => (
+          <li
+            className="list-group-item d-flex justify-content-between align-items-center"
+            key={item.id}
+          >
+            <span className="badge badge-primary col-1">
+              {item.category.name}
+            </span>
+            <span className="col-4">{item.title}</span>
+            <span className="col-2 font-weight-bolder">
+              {item.category.type === "imooc" ? "+" : "-"}
+              {item.price}元
+            </span>
+            <span className="col-2">{item.dete}</span>
+            <button
+              type="button"
+              className="col-1 btn btn-outline-primary btn-sm"
+            >
+              编辑
+            </button>
+            <button
+              type="button"
+              className="col-1 btn btn-outline-danger btn-sm"
+            >
+              删除
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button type="button" className="my-4 btn btn-outline-secondary btn-sm btn-lg btn-block">创建新的记账记录</button>
+    </>
+  );
+};
 
 export default PriceList;
