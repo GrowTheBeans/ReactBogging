6565b05090dea37466b244e4d62916199105795a
粽子 <rail_0039@163.com>
2021-01-05 01:01:37 +0800

    feature: 路由配置和页面优化

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import PriceList from './components/PriceList/price-list.jsx';
 import List from './mock/PriceList.json';
 import bg from './images/bg.png';
+import { Link } from 'react-router-dom';
 import './App.scss';
 
 function App() {
@@ -11,12 +12,20 @@ function App() {
         <h2>React账本项目</h2>
         <p>从设计图到上线，精通组件化思维和组件测试，一步步进行迭代</p>
       </header>
-      {/*<nav className="container d-flex align-items-end text-center">*/}
-      {/*  23*/}
-      {/*</nav>*/}
+      <ul className="nav container nav-header justify-content-center">
+        <li className="nav-item">
+          <Link to="/" className="nav-link active">Active</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Link</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Disabled</Link>
+        </li>
+      </ul>
       <div className="container">
-        {/*onEditorItem={Editor} onDeleteItem={Delete}*/}
-        <PriceList PriceList={List}/>
+        {/* onEditorItem={Editor} onDeleteItem={Delete} */}
+        <PriceList PriceList={List} />
       </div>
     </div>
   );
